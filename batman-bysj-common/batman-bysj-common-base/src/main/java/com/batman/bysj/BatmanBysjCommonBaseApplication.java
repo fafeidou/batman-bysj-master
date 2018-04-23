79cf1e66cdff805464fadaec160044509c4bb21d
batman <943104990@qq.com>
2018-04-23 17:30:15 +0800

    add oss upload model

--- batman-bysj-common/batman-bysj-common-base/src/main/java/com/batman/bysj/BatmanBysjCommonBaseApplication.java
+++ batman-bysj-common/batman-bysj-common-base/src/main/java/com/batman/bysj/BatmanBysjCommonBaseApplication.java
@@ -1,13 +1,8 @@
 package com.batman.bysj;
 
-import com.batman.bysj.common.mail.batmanbysjcommonmail.Mail;
-import com.batman.bysj.common.mail.batmanbysjcommonmail.MailClient;
-import com.batman.bysj.common.mail.batmanbysjcommonmail.MailClientFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Collections;
 
 @SpringBootApplication
 public class BatmanBysjCommonBaseApplication implements CommandLineRunner {
@@ -18,15 +13,7 @@ public class BatmanBysjCommonBaseApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        MailClient mailClient = MailClientFactory.fromDefaultProperties();
 
-        Mail mail = new Mail()
-                .setSubject("Hello, World!")
-                .setContentType("text/html")
-                .setContent("<h1>This is a test mail!</h1>")
-                .setReceivers(Collections.singletonList("943104990@qq.com"));
-
-        mailClient.send(mail);
     }
 
 }
---
+++ batman-bysj-common/batman-bysj-common-oss/src/main/java/com/batman/bysj/common/oss/cloud/QiniuCloudStorageService.java
@@ -0,0 +1,103 @@
+/*
+ *
+ *  *  Copyright (C) 2018  Wanghaobin<463540703@qq.com>
+ *
+ *  *  AG-Enterprise 企业版源码
+ *  *  郑重声明:
+ *  *  如果你从其他途径获取到，请告知老A传播人，奖励1000。
+ *  *  老A将追究授予人和传播人的法律责任!
+ *
+ *  *  This program is free software; you can redistribute it and/or modify
+ *  *  it under the terms of the GNU General Public License as published by
+ *  *  the Free Software Foundation; either version 2 of the License, or
+ *  *  (at your option) any later version.
+ *
+ *  *  This program is distributed in the hope that it will be useful,
+ *  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *  GNU General Public License for more details.
+ *
+ *  *  You should have received a copy of the GNU General Public License along
+ *  *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+package com.batman.bysj.common.oss.cloud;
+
+import com.batman.bysj.common.oss.config.CloudStorageConfig;
+import com.qcloud.cos.utils.IOUtils;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 七牛云存储
+* @author victor.qin
+* @date 2018/4/23 17:14
+*/
+public class QiniuCloudStorageService extends CloudStorageService {
+    private UploadManager uploadManager;
+    private String token;
+
+    public QiniuCloudStorageService(CloudStorageConfig config) {
+        this.config = config;
+
+        //初始化
+        init();
+    }
+
+    private void init() {
+        uploadManager = new UploadManager(new Configuration(Zone.autoZone()));
+        token = Auth.create(config.getQiniuAccessKey(), config.getQiniuSecretKey()).
+                uploadToken(config.getQiniuBucketName());
+    }
+
+    @Override
+    public String upload(byte[] data, String path) {
+        try {
+            Response res = uploadManager.put(data, path, token);
+            if (!res.isOK()) {
+                throw new RuntimeException("上传七牛出错：" + res.toString());
+            }
+        } catch (Exception e) {
+            try {
+                throw new Exception("上传文件失败，请核对七牛配置信息");
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        return config.getQiniuDomain() + "/" + path;
+    }
+
+    @Override
+    public String upload(InputStream inputStream, String path) {
+        try {
+            byte[] data = IOUtils.toByteArray(inputStream);
+            return this.upload(data, path);
+        } catch (IOException e) {
+            try {
+                throw new Exception("上传文件失败");
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String uploadSuffix(byte[] data, String suffix) {
+        return upload(data, getPath(config.getQiniuPrefix(), suffix));
+    }
+
+    @Override
+    public String uploadSuffix(InputStream inputStream, String suffix) {
+        return upload(inputStream, getPath(config.getQiniuPrefix(), suffix));
+    }
+}
