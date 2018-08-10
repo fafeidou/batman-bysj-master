55d96cafff50deb58ab37123432f4e9e79e1c55b
batman <943104990@qq.com>
2018-08-10 19:31:43 +0800

    es add

--- batman-bysj-common/batman-bysj-common-es/src/main/java/com/batman/bysj/es/config/ElasticConfig.java
+++ batman-bysj-common/batman-bysj-common-es/src/main/java/com/batman/bysj/es/config/ElasticConfig.java
@@ -1,13 +1,68 @@
 package com.batman.bysj.es.config;
 
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
 
 /**
  * @author victor.qin
  * @date 2018/7/28 18:01
  */
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.batman.bysj.es")
 public class ElasticConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ElasticConfig.class);
+
+    /**
+     * elk集群地址
+     */
+    @Value("${elasticsearch.ip}")
+    private String hostName;
+    /**
+     * 端口
+     */
+    @Value("${elasticsearch.port}")
+    private String port;
+    /**
+     * 集群名称
+     */
+    @Value("${elasticsearch.cluster.name}")
+    private String clusterName;
+
+    /**
+     * 连接池
+     */
+    @Value("${elasticsearch.pool}")
+    private String poolSize;
+
+    @Bean
+    public TransportClient init() {
+
+        TransportClient transportClient = null;
+
+        try {
+            // 配置信息
+            Settings esSetting = Settings.builder()
+                    .put("cluster.name", clusterName)
+                    .put("client.transport.sniff", true)//增加嗅探机制，找到ES集群
+                    .put("thread_pool.search.size", Integer.parseInt(poolSize))//增加线程池个数，暂时设为5
+                    .build();
+
+            transportClient = new PreBuiltTransportClient(esSetting);
+            InetSocketTransportAddress inetSocketTransportAddress = new InetSocketTransportAddress(InetAddress.getByName(hostName), Integer.valueOf(port));
+            transportClient.addTransportAddresses(inetSocketTransportAddress);
+
+        } catch (Exception e) {
+            LOGGER.error("elasticsearch TransportClient create error!!!", e);
+        }
+
+        return transportClient;
+    }
 }
